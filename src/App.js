157351d6fda6e8b460360017983b825b8c4d5226
Jaki1965 <igor.kiryanov@mail.ru>
2023-07-01 23:58:28 +0300

    commit 11

--- src/App.js
+++ src/App.js
@@ -5,9 +5,36 @@ function App() {
   return (
     <>
       <div className="App">
-        <h1 className='App_title'>
-        ЗДЕСЬ БУДЕТ ПРОЕКТ useRef
-        </h1>
+        <p className='App_title'>
+        ЗДЕСЬ ПРИМЕР ОДИН по применеию useRef
+        </p>
+        <div className='App_block_one'>
+
+        </div>
+        <div className='App_block_two'>
+
+        </div>
+        <div className='App_block_three'>
+
+        </div>
+        <div className='App_block_four'>
+
+        </div>
+        <div className='App_buttons'>
+          <button type="button" className='App_button'>
+            Я кнопка один
+          </button>
+          <button type="button" className='App_button'>
+            Я кнопка два 
+          </button>
+          <button type="button" className='App_button'>
+            Я кнопка три 
+          </button>
+          <button type="button" className='App_button'>
+            Я кнопка четыре 
+          </button>
+        </div>
+       
       </div>
     </>
   );
